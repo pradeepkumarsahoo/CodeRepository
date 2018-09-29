21c00d1a4f3415f2b39f36cd9abdc23a4260c17d
pradeepkumarsahoo <pradeepsahoo.blr@gmail.com>
2018-09-29 08:37:29 +0530

    Angular log in app

---
+++ loginapp/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes:Routes = [
+  {
+    
+      path: '',
+     
+      component: LoginFormComponent
+    },
+    
+  {
+    path: 'dashboard',
+   
+    component: DashboardComponent
+  }
+  
+]
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginFormComponent,
+    DashboardComponent
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+
+
+export class AppModule { }
---
+++ loginapp/src/app/login-form/login-form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+  errormessage:string;
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+    console.log('hit');
+  }
+
+  loginUser(e) {
+  	e.preventDefault();
+  	console.log(e);
+  	var username = e.target.elements[0].value;
+  	var password = e.target.elements[1].value;
+  	
+  	if(username == 'admin' && password == 'admin') {
+      
+  		this.router.navigate(['dashboard']);
+    }
+    
+    else{
+
+this.errormessage="User Name and Password not correct !"
+    }
+  }
+
+}
