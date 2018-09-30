62561ac8bdf024f82de11f869aaaf4b9f5e98af4
pradeepkumarsahoo <pradeepsahoo.blr@gmail.com>
2018-09-30 13:19:08 +0530

    service demo

--- Servicedemo/src/app/app.component.ts
+++ Servicedemo/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component,OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { CommonDataService } from './common-data.service';
-import { IEmployee } from './Employee';
+
 
 
 @Component({
@@ -11,33 +11,19 @@ import { IEmployee } from './Employee';
 })
 export class AppComponent {
   title = 'app';
-  messageName :string ;
-  employees: IEmployee[];
+  messageName: string;
+ 
+  result: number;
+  MaskedAccount: any;
 
+  constructor(private _CommonDataService: CommonDataService) {
 
-  pageTitle: string = "Employee Details";
-    imageWidth: number = 50;
-    imageHeight: number = 50;
-    filterByName: string = "";
-    showImageColumn: boolean = false;
-    eRating: string = "";
-   
-    errorMessage: string;
-
-  constructor(private _CommonDataService :CommonDataService)
-  {
-
-    //alert("called ...");
-this.messageName=_CommonDataService.GetData();
-//this.employees=_CommonDataService.getEmployees();
+    this.messageName = _CommonDataService.GetData();
   }
 
-  ngOnInit(): void {
-    // let self = this;
-    // self._CommonDataService.getEmployees().subscribe(empData => this.employees = empData,
-    //     error => this.errorMessage = <any>error);
-}
+  MaskAccount(accNo) {
 
+    this.MaskedAccount = this._CommonDataService.MaskAccountNumber(accNo);
 
-
+  }
 }
--- Servicedemo/src/app/common-data.service.ts
+++ Servicedemo/src/app/common-data.service.ts
@@ -1,84 +1,21 @@
 import { Injectable } from '@angular/core';
-import { IEmployee } from './Employee';  
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CommonDataService {
 
-  constructor() { }
+    constructor() { }
 
-  GetData()
+    GetData() {
+        return "My Name is :xyz";
+    }
 
-  {
-    return "My Name is :xyz";
-  }
+    MaskAccountNumber(str) {
 
-  public getEmployees(): IEmployee[] {  
-    return [  
-        {  
-            "empId": "E001",  
-            "name": "Leanne Graham",  
-            "email": "Sincere@april.biz",  
-            "phone": "1-770-736-8031 x56442",  
-            "photo": "./app/assets/imgs/face1.jpg",  
-            "salary": 50000,  
-            "appraisalRating": 4  
-        },  
-        {  
-            "empId": "E002",  
-            "name": "Ervin Howell",  
-            "email": "Shanna@melissa.tv",  
-            "phone": "210.067.6132",  
-            "photo": "./app/assets/imgs/face2.jpg",  
-            "salary": 65000,  
-            "appraisalRating": 3.5  
-        },  
-        {  
-            "empId": "E003",  
-            "name": "Clementine Bauch",  
-            "email": "Nathan@yesenia.net",  
-            "phone": "1-477-935-8478 x6430",  
-            "photo": "./app/assets/imgs/face3.jpg",  
-            "salary": 62500,  
-            "appraisalRating": 3.75  
-        },  
-        {  
-            "empId": "E004",  
-            "name": "Patricia Lebsack",  
-            "email": "Julianne.OConner@kory.org",  
-            "phone": "(254)954-1289",  
-            "photo": "./app/assets/imgs/face4.png",  
-            "salary": 49500,  
-            "appraisalRating": 4.25  
-        },  
-        {  
-            "empId": "E005",  
-            "name": "Chelsey Dietrich",  
-            "email": "Lucio_Hettinger@annie.ca",  
-            "phone": "493-170-9623 x156",  
-            "photo": "./app/assets/imgs/face5.jpg",  
-            "salary": 25000,  
-            "appraisalRating": 3.25  
-        },  
-        {  
-            "empId": "E006",  
-            "name": "Mrs. Dennis Schulist",  
-            "email": "Karley_Dach@jasper.info",  
-            "phone": "1-463-123-4447",  
-            "photo": "./app/assets/imgs/face6.ico",  
-            "salary": 100000,  
-            "appraisalRating": 4.5  
-        },  
-        {  
-            "empId": "E007",  
-            "name": "Kurtis Weissnat",  
-            "email": "Telly.Hoeger@billy.biz",  
-            "phone": "010-692-6593 x09125",  
-            "photo": "./app/assets/imgs/face7.png",  
-            "salary": 36900,  
-            "appraisalRating": 4  
-        }  
-    ];  
-}  
+        var trailingCharsIntactCount = 4;
+        str = new Array(str.length - trailingCharsIntactCount + 1).join('x') + str.slice(-trailingCharsIntactCount);
+        return str;
+    }
 }
